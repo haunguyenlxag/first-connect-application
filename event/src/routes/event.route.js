409505e1ce91b3bf9a3f92b24b97c9fd1d30198f
Hau Nguyen <Hau.Nguyen@isobar.com>
2024-04-19 16:56:11 +0700

    writelog

--- event/src/routes/event.route.js
+++ event/src/routes/event.route.js
@@ -5,7 +5,7 @@ import { post } from '../controllers/event.controller.js';
 const eventRouter = Router();
 
 eventRouter.post('/', async (req, res) => {
-  logger.info('Event message received up');
+  logger.info('Event message received up2');
   await post(req, res);
   //res.status(200);
   //res.send();
